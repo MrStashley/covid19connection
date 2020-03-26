dc8915b08c84029c17cab117538f2d038301d1fc
MrStashley <colel1410@gmail.com>
2020-03-26 15:35:51 -0400

    adding database connection

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'mustache');
 app.engine('mustache', require('hogan-middleware').__express);
 
+const con = mysql.createPool({
+	connectionLimit: 100,
+	host: "us-cdbr-iron-east-01.cleardb.net",
+	user: "b101f02dd54e99",
+	password:"dd79f6d8",
+	port: 3306,
+	database: "heroku_4a5132c4edcc681",
+	debug: 'false',
+	connectTimeout: 100000
+})
+
 class dataHolder {
   constructor(lat, long, accuracy, id){
     this.lat = lat;
@@ -23,10 +34,8 @@ class dataHolder {
   }
 }
 
-var data = [];
-
 app.get("/", (req, res, next) =>{
-  res.render("home", {data: JSON.stringify(data) });
+  res.render("home");
 });
 
 app.get("/color", (req, res, next) =>{
